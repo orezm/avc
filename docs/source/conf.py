b270b834da474b9dd2eb40dc67417d0d3c4ade26
orezm <90485461+orezm@users.noreply.github.com>
2021-09-15 02:02:25 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Awesome VPN'
+copyright = '2021, www.orezm.com'
+author = 'Ernest Gingrich'
 
 release = '0.1'
 version = '0.1.0'
